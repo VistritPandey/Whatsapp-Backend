5b4065248dad6ab2eaa4b2ebed968fe57899db5b
vistritpandey <vistritpandey@gmail.com>
2020-11-09 16:38:30 +0530

    okay didn't work

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
-const port = process.env.PORT || 9000
+const app = express();
+const port = process.env.PORT || 9000;
 
-
-
-
-
-
-
-
-
-
+const connection_url ='mongodb+srv://admin:<password>@vistrit.ipx1s.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+mongoose.connect(connection_url,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 
 
 
-app.get('/',(req,res)=>res.status(200).send('testing'));
 
-app.listen(port,()=>console.log(`Listening on Localhost:${port}`));
+
+
+
+
+
+
+app.get("/", (req ,res ) => res.status(200).send("test"));
+
+app.listen(port, ()=>console.log(`Listening on Localhost:${port}`));
